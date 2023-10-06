34d0be752315a9acab8933421c8078cef8e18de7
yuzu584 <yuzuki@hallo.com>
2023-10-06 22:07:54 +0900

    惑星と衝突した際に惑星を加速させる機能を追加

--- Assets/Script/Shot.cs
+++ Assets/Script/Shot.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 // 発射ボタンで弾を発射する
 public class Shot : MonoBehaviour
 {
-    [SerializeField] CreateRay Cr;  // RayとLineを作る関数の型
-    public float speed = 1.0f;      // 移動速度
-    public static float charge = 0; // 球のチャージ
-    public float chargeSpeed = 10;  // 球のチャージ速度
-    public int bouncePower = 100;   // 衝突したときの反発力
+    [SerializeField] CreateRay Cr;       // RayとLineを作る関数の型
+    public float speed = 1.0f;           // 移動速度
+    public static float charge = 0;      // 球のチャージ
+    public float chargeSpeed = 10;       // 球のチャージ速度
+    public int playerBouncePower = 100;  // 衝突したときのプレイヤーの反発力
+    public int planetBouncePower = 100;  // 衝突したときの惑星の反発力
 
-    Camera cam;              // カメラ
-    Vector3 direction;              // 向き
-    Rigidbody rb;                   // プレイヤーのRigidbody
-    RaycastHit hit;                 // Rayのhit
+    Camera cam;         // カメラ
+    Vector3 direction;  // 向き
+    Rigidbody rb;       // プレイヤーのRigidbody
+    Rigidbody cRb;      // 衝突したオブジェクトのRigidbody
+    RaycastHit hit;     // Rayのhit
 
     void Start()
     {
@@ -32,7 +34,17 @@ public class Shot : MonoBehaviour
         rb.velocity *= 0;
 
         // 力を加える
-        rb.AddForce(direction * speed * bouncePower);
+        rb.AddForce(direction * speed * playerBouncePower);
+
+        // 衝突したオブジェクトのタグがPlanetなら
+        if (collision.gameObject.tag == "Planet")
+        {
+            // 衝突したオブジェクトのrigidbodyを取得
+            cRb = collision.gameObject.GetComponent<Rigidbody>();
+
+            // 加速させる
+            cRb.velocity *= planetBouncePower / 50;
+        }
     }
 
     // 衝突後に次の角度を設定
