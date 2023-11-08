b0606126e23d515b01ed50b989291a0851e60a77
Yuzuki <yuzuki@hallo.com>
2023-11-08 11:39:04 +0900

    UIControllerのクラスをUIの種類ごとに分割

--- Assets/Script/UIController.cs
+++ Assets/Script/UIController.cs
@@ -8,40 +8,69 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     // InspectorでUIの配列を指定
-    [SerializeField] InGameUI inGameUI;
-    [SerializeField] PauseUI pauseUI;
-
-    // UIの配列
-    [System.Serializable]
-    public class InGameUI
-    {
-        public GameObject chargeUI;          // チャージのUI
-        public Image chargeCircle;           // チャージの円
-        public Text chargeValue;             // チャージの数値
-        public Text chargeName;              // チャージの文字
-        public Image EnergyGauge;            // エネルギーゲージ
-        public Image EnergyAfterImage;       // エネルギーゲージの減少量
-        public Image EnergyGaugeOutline;     // エネルギーゲージの枠
-        public Text EnergyValue;             // エネルギーの数値
-        public GameObject Message;           // メッセージ
-        public Text NoEnergy;                // エネルギーがない旨を伝えるテキスト
-        public Text skillName;               // スキル名
-        public Image skillGauge;             // 効果時間とクールダウンのゲージ
-        public GameObject planetInfo;        // 惑星情報UI
-        public Image planetInfoRing;         // 惑星情報UIの円
-        public LineRenderer planetInfoLine;  // 惑星情報UIの線
-        public Text planetName;              // 惑星の名前
-        public Image reticle;                // レティクル
-    }
+    public ChargeUI chargeUI;
+    public EnergyUI energyUI;
+    public MessageUI messageUI;
+    public SkillUI skillUI;
+    public PlanetInfoUI planetInfoUI;
+    public PauseUI pauseUI;
+    public OtherUI otherUI;
 
     [System.Serializable]
     public class PauseUI
     {
-        public GameObject pauseUI; // ポーズ画面のUI
+        public GameObject pauseUI;           // ポーズ画面のUI
     }
 
-    public int popupAmount = 0;                // ポップアップの数
-    public bool[] drawingPopup = new bool[10]; // ポップアップが描画されているか
+    [System.Serializable]
+    public class ChargeUI
+    {
+        public GameObject allChargeUI;       // 全てのチャージのUI
+        public Image chargeCircle;           // チャージの円
+        public Text chargeValue;             // チャージの数値
+        public Text chargeName;              // チャージの文字
+    }
+
+    [System.Serializable]
+    public class EnergyUI
+    {
+        public Image EnergyGauge;            // エネルギーゲージ
+        public Image EnergyAfterImage;       // エネルギーゲージの減少量
+        public Image EnergyGaugeOutline;     // エネルギーゲージの枠
+        public Text EnergyValue;             // エネルギーの数値
+    }
+
+    [System.Serializable]
+    public class MessageUI
+    {
+        public GameObject Message;           // メッセージ
+        public Text NoEnergy;                // エネルギーがない旨を伝えるテキスト
+    }
+
+    [System.Serializable]
+    public class SkillUI
+    {
+        public Text skillName;               // スキル名
+        public Image skillGauge;             // 効果時間とクールダウンのゲージ
+    }
+
+    [System.Serializable]
+    public class PlanetInfoUI
+    {
+        public GameObject allPlanetInfo;     // 惑星情報UI
+        public Image targetRing;             // 惑星情報UIの円
+        public LineRenderer planetInfoLine;  // 惑星情報UIの線
+        public Text planetName;              // 惑星の名前
+    }
+
+    [System.Serializable]
+    public class OtherUI
+    {
+        public Image reticle;                // レティクル
+    }
+
+    [System.NonSerialized] public int popupAmount = 0;                // ポップアップの数
+    [System.NonSerialized] public bool[] drawingPopup = new bool[10]; // ポップアップが描画されているか
 
     [SerializeField] private Shot shot;                                   // Shot型の変数
     [SerializeField] private EnergyController energyController;           // EnergyController型の変数
@@ -58,37 +87,37 @@ public class UIController : MonoBehaviour
     void DrawEnergyUI()
     {
         // エネルギーゲージの増減を描画
-        inGameUI.EnergyGauge.fillAmount = energyController.energy / energyController.maxEnergy;
+        energyUI.EnergyGauge.fillAmount = energyController.energy / energyController.maxEnergy;
 
-        if (inGameUI.EnergyAfterImage.fillAmount > energyController.energy / energyController.maxEnergy)
+        if (energyUI.EnergyAfterImage.fillAmount > energyController.energy / energyController.maxEnergy)
         {
             // エネルギーゲージの減少量を少しずつ減らす
-            inGameUI.EnergyAfterImage.fillAmount -=
-                (inGameUI.EnergyAfterImage.fillAmount - energyController.energy / energyController.maxEnergy) * Time.deltaTime;
+            energyUI.EnergyAfterImage.fillAmount -=
+                (energyUI.EnergyAfterImage.fillAmount - energyController.energy / energyController.maxEnergy) * Time.deltaTime;
         }
 
         // エネルギーの数値を表示
-        inGameUI.EnergyValue.text = energyController.energy.ToString("0");
+        energyUI.EnergyValue.text = energyController.energy.ToString("0");
 
         // エネルギーが0以下かつ非表示なら
-        if ((energyController.energy <= 0) && (inGameUI.NoEnergy.enabled == false))
+        if ((energyController.energy <= 0) && (messageUI.NoEnergy.enabled == false))
         {
             // エネルギーゲージの枠と数値を赤色にする
-            inGameUI.EnergyGaugeOutline.color = new Color32(155, 0, 0, 100);
-            inGameUI.EnergyValue.color = new Color32(155, 0, 0, 100);
+            energyUI.EnergyGaugeOutline.color = new Color32(155, 0, 0, 100);
+            energyUI.EnergyValue.color = new Color32(155, 0, 0, 100);
 
             // エネルギーがない旨を伝えるテキストを表示
-            inGameUI.NoEnergy.enabled = true;
+            messageUI.NoEnergy.enabled = true;
         }
         // エネルギーが0より上かつ表示されているなら
-        else if ((energyController.energy > 0) && (inGameUI.NoEnergy.enabled == true))
+        else if ((energyController.energy > 0) && (messageUI.NoEnergy.enabled == true))
         {
             // エネルギーゲージの枠と数値を白色にする
-            inGameUI.EnergyGaugeOutline.color = new Color32(255, 255, 255, 100);
-            inGameUI.EnergyValue.color = new Color32(255, 255, 255, 255);
+            energyUI.EnergyGaugeOutline.color = new Color32(255, 255, 255, 100);
+            energyUI.EnergyValue.color = new Color32(255, 255, 255, 255);
 
             // エネルギーがない旨を伝えるテキストを非表示
-            inGameUI.NoEnergy.enabled = false;
+            messageUI.NoEnergy.enabled = false;
         }
     }
 
@@ -99,23 +128,23 @@ public class UIController : MonoBehaviour
         if (shot.charge > 0)
         {
             // チャージのUIが無効化されていたら
-            if (!(inGameUI.chargeUI.activeSelf))
+            if (!(chargeUI.allChargeUI.activeSelf))
             {
                 // UIを有効化
-                inGameUI.chargeUI.SetActive(true);
+                chargeUI.allChargeUI.SetActive(true);
             }
 
             // チャージの数値をテキストで表示
-            inGameUI.chargeValue.text = shot.charge.ToString("0") + "%";
+            chargeUI.chargeValue.text = shot.charge.ToString("0") + "%";
 
             // チャージの円を描写
-            inGameUI.chargeCircle.fillAmount = shot.charge / 100;
+            chargeUI.chargeCircle.fillAmount = shot.charge / 100;
         }
         // チャージされていないかつ表示されているなら
-        else if ((shot.charge == 0) && (inGameUI.chargeUI.activeSelf))
+        else if ((shot.charge == 0) && (chargeUI.allChargeUI.activeSelf))
         {
             // UIを無効化
-            inGameUI.chargeUI.SetActive(false);
+            chargeUI.allChargeUI.SetActive(false);
         }
     }
 
@@ -123,14 +152,14 @@ public class UIController : MonoBehaviour
     public void DrawSkillUI(string skillName, float coolDown, float effectTime, float nowCoolDown, float nowEffectTime)
     {
         // テキストを現在のスキル名に変更
-        inGameUI.skillName.text = skillName;
+        skillUI.skillName.text = skillName;
 
         // 効果時間を描画
         if (nowEffectTime > 0)
-            inGameUI.skillGauge.fillAmount = nowEffectTime / effectTime;
+            skillUI.skillGauge.fillAmount = nowEffectTime / effectTime;
         // 効果時間が経過していたならクールダウンを描画
         else if (nowCoolDown > 0)
-            inGameUI.skillGauge.fillAmount = (coolDown - nowCoolDown) / coolDown;
+            skillUI.skillGauge.fillAmount = (coolDown - nowCoolDown) / coolDown;
     }
 
     // 惑星情報UIを描画
@@ -140,33 +169,33 @@ public class UIController : MonoBehaviour
         if(screenController.screenNum == 0)
         {
             // UIが非表示なら表示
-            if(!(inGameUI.planetInfo.activeSelf))
+            if(!(planetInfoUI.allPlanetInfo.activeSelf))
             {
-                inGameUI.planetInfo.SetActive(true);
+                planetInfoUI.allPlanetInfo.SetActive(true);
             }
             // 惑星情報UIの円のスクリーン座標を変更
-            inGameUI.planetInfoRing.rectTransform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, position);
+            planetInfoUI.targetRing.rectTransform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, position);
 
             // 惑星情報UIの線のスクリーン座標をワールド座標に変換
-            PIL1 = Camera.main.ScreenToWorldPoint(inGameUI.planetInfoRing.rectTransform.position + new Vector3(0, 0, 10));
-            PIL2 = Camera.main.ScreenToWorldPoint(inGameUI.planetInfoRing.rectTransform.position + new Vector3(50, 50, 10));
-            PIL3 = Camera.main.ScreenToWorldPoint(inGameUI.planetInfoRing.rectTransform.position + new Vector3(150, 50, 10));
+            PIL1 = Camera.main.ScreenToWorldPoint(planetInfoUI.targetRing.rectTransform.position + new Vector3(0, 0, 10));
+            PIL2 = Camera.main.ScreenToWorldPoint(planetInfoUI.targetRing.rectTransform.position + new Vector3(50, 50, 10));
+            PIL3 = Camera.main.ScreenToWorldPoint(planetInfoUI.targetRing.rectTransform.position + new Vector3(150, 50, 10));
 
             // 線を描画
-            inGameUI.planetInfoLine.SetPosition(0, PIL1);
-            inGameUI.planetInfoLine.SetPosition(1, PIL2);
-            inGameUI.planetInfoLine.SetPosition(2, PIL3);
+            planetInfoUI.planetInfoLine.SetPosition(0, PIL1);
+            planetInfoUI.planetInfoLine.SetPosition(1, PIL2);
+            planetInfoUI.planetInfoLine.SetPosition(2, PIL3);
 
             // 惑星の名前をテキストに設定
-            inGameUI.planetName.text = planetName;
+            planetInfoUI.planetName.text = planetName;
 
             // 惑星の名前UIの位置を設定
-            inGameUI.planetName.rectTransform.position = inGameUI.planetInfoRing.rectTransform.position + new Vector3(160, 80, 10);
+            planetInfoUI.planetName.rectTransform.position = planetInfoUI.targetRing.rectTransform.position + new Vector3(160, 80, 10);
         }
         // ポーズ画面かつUIが表示されているなら非表示にする
-        else if((screenController.screenNum == 1) && (inGameUI.planetInfo.activeSelf))
+        else if((screenController.screenNum == 1) && (planetInfoUI.allPlanetInfo.activeSelf))
         {
-            inGameUI.planetInfo.SetActive(false);
+            planetInfoUI.allPlanetInfo.SetActive(false);
         }
     }
 
@@ -180,7 +209,7 @@ public class UIController : MonoBehaviour
         postProcessController.DepthOfFieldOnOff(draw);
 
         // レティクルを表示又は非表示
-        inGameUI.reticle.enabled = !(draw);
+        otherUI.reticle.enabled = !(draw);
     }
 
     // 惑星が破壊された旨を伝えるポップアップを描画
@@ -205,7 +234,7 @@ public class UIController : MonoBehaviour
         GameObject popup = Instantiate(popUp);
 
         // 親を設定
-        popup.transform.SetParent(inGameUI.Message.transform, false);
+        popup.transform.SetParent(messageUI.Message.transform, false);
 
         // 位置を設定
         popup.transform.position += new Vector3(0, i * -40, 0);
@@ -232,17 +261,17 @@ public class UIController : MonoBehaviour
     void Start()
     {
         // 惑星情報UIの円のRectTransformを取得
-        PIR = inGameUI.planetInfoRing.GetComponent<RectTransform>();
+        PIR = planetInfoUI.targetRing.GetComponent<RectTransform>();
 
         // 惑星情報UIの線の始点と終点の太さを指定
-        inGameUI.planetInfoLine.startWidth = 0.01f;
-        inGameUI.planetInfoLine.endWidth = 0.01f;
+        planetInfoUI.planetInfoLine.startWidth = 0.01f;
+        planetInfoUI.planetInfoLine.endWidth = 0.01f;
 
         // 惑星情報UIの線の数
-        inGameUI.planetInfoLine.positionCount = 3;
+        planetInfoUI.planetInfoLine.positionCount = 3;
 
         // エネルギーがない旨を伝えるテキストを非表示
-        inGameUI.NoEnergy.enabled = false;
+        messageUI.NoEnergy.enabled = false;
 
         // ポーズ画面のUIを非表示
         DrawPauseUI(false);
