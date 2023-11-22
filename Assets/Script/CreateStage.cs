6d4df3fd232787f8318ca2b336531fea193e08ed
Yuzuki <yuzuki@hallo.com>
2023-11-22 15:07:05 +0900

    スフィアを表示/非表示する機能を追加
    星のインスタンスの親を設定

--- Assets/Script/CreateStage.cs
+++ Assets/Script/CreateStage.cs
@@ -7,6 +7,7 @@ public class CreateStage : MonoBehaviour
 {
     [SerializeField] StageData stageData;             // InspectorでStageDataを指定
     [SerializeField] StageController stageController; // InspectorでStageControllerを指定
+    [SerializeField] GameObject sphere;               // スフィア
 
     private GameObject[] star;   // 星のGameObject
     private GameObject[] planet; // 惑星のGameObject
@@ -18,19 +19,24 @@ public class CreateStage : MonoBehaviour
         star = new GameObject[stageData.stageList[stageController.stageNum].fixedStar.Length];
         planet = new GameObject[stageData.stageList[stageController.stageNum].planet.Length];
 
-        // 恒星のインスタンスを生成・名前から(clone)を削除
+        // 恒星のインスタンスを生成・名前から(clone)を削除・親を設定
         for(int i = 0; i < stageData.stageList[stageController.stageNum].fixedStar.Length; i++)
         {
             star[i] = Instantiate(stageData.stageList[stageController.stageNum].fixedStar[i]);
             star[i].name = stageData.stageList[stageController.stageNum].fixedStar[i].name;
+            star[i].transform.SetParent(this.transform, false);
         }
 
-        // 惑星のインスタンスを生成・名前から(clone)を削除
+        // 惑星のインスタンスを生成・名前から(clone)を削除・親を設定
         for (int i = 0; i < stageData.stageList[stageController.stageNum].planet.Length; i++)
         {
             planet[i] = Instantiate(stageData.stageList[stageController.stageNum].planet[i]);
             planet[i].name = stageData.stageList[stageController.stageNum].planet[i].name;
+            planet[i].transform.SetParent(this.transform, false);
         }
+
+        // スフィアを表示
+        sphere.SetActive(true);
     }
 
     // ステージを削除
@@ -49,5 +55,14 @@ public class CreateStage : MonoBehaviour
             Destroy(planet[i]);
             planet[i] = null;
         }
+
+        // スフィアを非表示
+        sphere.SetActive(false);
+    }
+
+    void Start()
+    {
+        // スフィアを非表示
+        sphere.SetActive(false);
     }
 }
