da10af38779be062fcdb48e3b9d50086d15519ad
Yuzuki <yuzuki@hallo.com>
2024-01-17 15:10:01 +0900

    スキルスロットを管理するスクリプトを追加
    スキルスロットのプレハブを追加
    その他細かい変更

---
+++ Assets/Script/UI/SkillSlotController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+// スキルスロットを管理
+public class SkillSlotController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [SerializeField] private Image image;          // スキルスロットの画像
+    [SerializeField] private Image imageOutline;   // スキルスロットの枠画像
+    [SerializeField] private Color OnPointerColor; // ポインターが乗った時の色
+    [SerializeField] private Color defaultColor;   // デフォルトの色
+    [SerializeField] private float fadeTime;       // フェード時間
+
+    // Findで探すもの
+    private GameObject UIFunctionController;
+
+    // Findで探したGameObjectのコンポーネント
+    private Lerp lerp;
+
+    // マウスポインターがボタンの上に乗ったら
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        // ボタンのアニメーション
+        StopAllCoroutines();
+        StartCoroutine(lerp.ChangeColor(image, defaultColor, OnPointerColor, fadeTime));
+    }
+
+    // マウスポインターがボタンの上から離れたら
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        // ボタンのアニメーション
+        StopAllCoroutines();
+        StartCoroutine(lerp.ChangeColor(image, OnPointerColor, defaultColor, fadeTime));
+    }
+
+    // ボタンがクリックされたら
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+
+    }
+
+    void Start()
+    {
+        // GameObjectを探す
+        UIFunctionController = GameObject.Find("UIFunctionController");
+
+        // 探したGameObjectのコンポーネントを取得
+        lerp = UIFunctionController.GetComponent<Lerp>();
+
+
+    }
+}
