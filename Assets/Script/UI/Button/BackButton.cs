1610ccc44dbf022c85f5b53e085700a1187ebd8f
yuzu584 <yuzuki@hallo.com>
2024-02-05 23:39:52 +0900

    戻るボタンの色が変わらないバグを修正
    クリック後に色が初期化されるよう修正

--- Assets/Script/UI/Button/BackButton.cs
+++ Assets/Script/UI/Button/BackButton.cs
@@ -11,24 +11,30 @@ public class BackButton : Button
     [SerializeField] private Image backGround;          // 背景画像
     [SerializeField] private Text text;                 // テキスト
 
-    private int oldScreen = 1;                          // 前回のスクリーン(戻り先の画面)
-    private Color startColor = new Color(0, 0, 0, 0);   // 変化前の色
-    private Color endColor = new Color (0, 0, 0, 0.1f); // 変化後の色
+    private int oldScreen = 1;                                    // 前回のスクリーン(戻り先の画面)
+    private Color startColor = new Color(1.0f, 1.0f, 1.0f, 0.0f); // 変化前の色
+    private Color endColor = new Color (1.0f, 1.0f, 1.0f, 0.1f);  // 変化後の色
+    private Color nowColor;                                       // 現在の色
+    private float fadeTime = 0.1f;                                // フェード時間
 
     // マウスポインターが乗った時の処理
     public override void EnterProcess()
     {
         // ボタンのアニメーション
+        nowColor = backGround.color;
+
         StopAllCoroutines();
-        lerp.Color_Image(backGround, startColor, endColor, 0.2f);
+        StartCoroutine(lerp.Color_Image(backGround, nowColor, endColor, fadeTime));
     }
 
     // マウスポインターが離れたときの処理
     public override void ExitProcess()
     {
         // ボタンのアニメーション
+        nowColor = backGround.color;
+
         StopAllCoroutines();
-        lerp.Color_Image(backGround, endColor, startColor, 0.2f);
+        StartCoroutine(lerp.Color_Image(backGround, nowColor, startColor, fadeTime));
     }
 
     // クリックされたときの処理
@@ -36,5 +42,8 @@ public class BackButton : Button
     {
         // 画面番号を前の画面にする
         screenController.screenNum = oldScreen;
+
+        // ボタンの色をリセット
+        backGround.color = startColor;
     }
 }
