a7d874400d327c14d86c0f8c85baae7302786d80
Yuzuki <yuzuki@hallo.com>
2024-02-20 11:40:59 +0900

    InputSystemを作り直し

--- Assets/Script/UI/PlanetInfoUIController.cs
+++ Assets/Script/UI/PlanetInfoUIController.cs
@@ -9,9 +9,9 @@ public class PlanetInfoUIController : MonoBehaviour
 {
     [SerializeField] private UIController uIController; // InspectorでUIControllerを指定
 
-    Vector3 PIL1; // 惑星情報UIの線の始点座標
-    Vector3 PIL2; // 惑星情報UIの線の中間座標
-    Vector3 PIL3; // 惑星情報UIの線の終点座標
+    Vector3 PILine1; // 惑星情報UIの線の始点座標
+    Vector3 PILine2; // 惑星情報UIの線の中間座標
+    Vector3 PILine3; // 惑星情報UIの線の終点座標
 
     // 惑星情報UIを描画
     public void DrawPlanetInfoUI(Vector3 position, string planetName)
@@ -20,14 +20,14 @@ public class PlanetInfoUIController : MonoBehaviour
         uIController.planetInfoUI.targetRing.rectTransform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, position);
 
         // 惑星情報UIの線のスクリーン座標をワールド座標に変換
-        PIL1 = Camera.main.ScreenToWorldPoint(uIController.planetInfoUI.targetRing.rectTransform.position + new Vector3(0, 0, 10));
-        PIL2 = Camera.main.ScreenToWorldPoint(uIController.planetInfoUI.targetRing.rectTransform.position + new Vector3(50, 50, 10));
-        PIL3 = Camera.main.ScreenToWorldPoint(uIController.planetInfoUI.targetRing.rectTransform.position + new Vector3(150, 50, 10));
+        PILine1 = Camera.main.ScreenToWorldPoint(uIController.planetInfoUI.targetRing.rectTransform.position + new Vector3(0, 0, 10));
+        PILine2 = Camera.main.ScreenToWorldPoint(uIController.planetInfoUI.targetRing.rectTransform.position + new Vector3(50, 50, 10));
+        PILine3 = Camera.main.ScreenToWorldPoint(uIController.planetInfoUI.targetRing.rectTransform.position + new Vector3(150, 50, 10));
 
         // 線を描画
-        uIController.planetInfoUI.planetInfoLine.SetPosition(0, PIL1);
-        uIController.planetInfoUI.planetInfoLine.SetPosition(1, PIL2);
-        uIController.planetInfoUI.planetInfoLine.SetPosition(2, PIL3);
+        uIController.planetInfoUI.planetInfoLine.SetPosition(0, PILine1);
+        uIController.planetInfoUI.planetInfoLine.SetPosition(1, PILine2);
+        uIController.planetInfoUI.planetInfoLine.SetPosition(2, PILine3);
 
         // 惑星の名前をテキストに設定
         uIController.planetInfoUI.planetName.text = planetName;
